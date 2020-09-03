53d9b85123f7869d8ef8cf0387570dc144a2f982
ethan2212 <e0407555@u.nus.edu>
2020-09-03 14:30:40 +0800

    A-CodeQuality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 
 public class Duke {
 
-    public static final String LOGO = "  _____ _   _                 \n" +
-            " | ____| |_| |__   __ _ _ __  \n" +
-            " |  _| | __| '_ \\ / _` | '_ \\ \n" +
+    public static final String LOGO = "  _____ _   _\n" +
+            " | ____| |_| |__   __ _ _ __\n" +
+            " |  _| | __| '_ \\ / _` | '_ \\\n" +
             " | |___| |_| | | | (_| | | | |\n" +
             " |_____|\\__|_| |_|\\__,_|_| |_|\n" +
             "\n";
     public static final String INTRO = " Hi there, I'm Ethan!\n" +
             " Do you need anything?\n";
     public static final String BYE = " Bye. See you soon!\n" +
-            "----------------------------BYE----------------------------\n";
+            "----------------------------BYE----------------------------";
     public static final String YOU = "----------------------------YOU----------------------------\n";
     public static final String ETHAN = "---------------------------ETHAN---------------------------\n";
 
