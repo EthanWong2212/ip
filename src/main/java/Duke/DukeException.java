92db589ade1214e71df01b0e3e762373863bc806
ethan2212 <e0407555@u.nus.edu>
2020-09-10 21:23:09 +0800

    Removed merge conflict messages

--- src/main/java/Duke/DukeException.java
+++ src/main/java/Duke/DukeException.java
@@ -2,10 +2,6 @@ package Duke;
 
 public class DukeException extends Exception{
     protected String error;
-<<<<<<< HEAD
-=======
-
->>>>>>> branch-Level-5
     public DukeException(String error){
         this.error=error;
     }
@@ -18,15 +14,12 @@ public class DukeException extends Exception{
         case "todo incomplete":
             System.out.println("OOPS!!! The description of a todo cannot be empty. :-(");
             break;
-<<<<<<< HEAD
-=======
         case "DL divider missing":
             System.out.println("OOPS!!! The divider (\"/\") for deadline is missing. :-(");
             break;
         case "DL incomplete":
             System.out.println("OOPS!!! The description and timing of a deadline cannot be empty. :-(");
             break;
->>>>>>> branch-Level-5
         }
     }
 }
