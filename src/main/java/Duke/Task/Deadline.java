5a8182118a11a59c2e35fd43a85171d40daa0928
ethan2212 <e0407555@u.nus.edu>
2020-09-29 22:41:25 +0800

    Level-8-Dates-and-Times

--- src/main/java/Duke/Task/Deadline.java
+++ src/main/java/Duke/Task/Deadline.java
@@ -1,21 +1,30 @@
 package Duke.Task;
 
-import Duke.DukeException;
+import Duke.Parser.Parser;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Deadline extends Task{
     protected String time;
+    protected LocalDateTime dateTime;
 
     public Deadline(String description, String time){
         super(description);
         this.time = time;
+        dateTime= Parser.parseDateTime(time);
     }
 
     public String getTime(){
         return time;
     }
 
+    public String stringDateTime(){
+        return this.dateTime.format(DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm"));
+    }
+
     @Override
     public String toString(){
-        return "[D] "+ super.toString()+ " (by: "+ time +")" ;
+        return "[D] "+ super.toString()+ " (by: "+  stringDateTime() +")" ;
     }
 }
